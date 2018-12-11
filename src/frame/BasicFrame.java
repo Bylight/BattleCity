7ba74a9c342302cd3440b60f90713a54d8517709
Bylight <javen_he@126.com>
2018-12-11 21:36:10 +0800

    v0.03 重写paint()画出代表坦克的实心圆

--- src/frame/BasicFrame.java
+++ src/frame/BasicFrame.java
@@ -1,19 +1,34 @@
 package frame;
 
+import java.awt.Color;
 import java.awt.Frame;
-import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
- * v0.02更新：
- * 	1. 使用setResizable()方法设置窗口大小不允许改变
- * 	2. 采用匿名类实现窗口关闭(重写WindowAdapter中的windowClosing方法)
- * 	3. 使用setTitle()方法设置窗口标题
+ * v0.03更新
+ * 	1. 重写paint()方法画出代表坦克的实心圆
+ * 	2. 使用setBackground()方法设置背景颜色
  * @author javen
  *
  */
 
 @SuppressWarnings("serial")
 public class BasicFrame extends Frame {
+	// 画出代表坦克的实心圆
+	// g为前景色
+	@Override
+	public void print(Graphics g) {
+		// TODO Auto-generated method stub
+		Color c = g.getColor();	// c用于保存g的初始颜色
+		
+		g.setColor(Color.RED);	// 设置颜色为红
+		g.fillOval(50, 50, 30, 30);	// 参数依次为x, y, width, height
+		
+		g.setColor(c); 	//恢复g的初始颜色
+	}
+
 	public void lauchFrame() {
 		setVisible(true);
 		setLocation(400, 300);
@@ -28,6 +43,8 @@ public class BasicFrame extends Frame {
 		});
 		//设置窗口标题
 		setTitle("坦克大战");
+		//设置背景颜色
+		setBackground(Color.GREEN);
 	}
 	
 }
